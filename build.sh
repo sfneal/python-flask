27261a926c5d54c05adf2efc5dfeb1ff7f13c9f3
mrstephenneal <stephen.neal14@gmail.com>
2019-08-16 14:44:32 -0400

    MAKE v2 image with git installed

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@
 docker build -t stephenneal/python-flask:3.7 ./3.7/
 docker build -t stephenneal/python-flask:3.7-alpine ./3.7-alpine/
 docker build -t stephenneal/python-flask:3.7-slim ./3.7-slim/
+docker build -t stephenneal/python-flask:3.7-slim-v2 ./3.7-slim-v2/
--- push.sh
+++ push.sh
@@ -5,3 +5,4 @@ sh ./build.sh
 docker push stephenneal/python-flask:3.7
 docker push stephenneal/python-flask:3.7-alpine
 docker push stephenneal/python-flask:3.7-slim
+docker push stephenneal/python-flask:3.7-slim-v2
