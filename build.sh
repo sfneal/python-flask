75cfdca5cf55cfc3a7d514f41bf756d3716a3a66
mrstephenneal <stephen.neal14@gmail.com>
2019-08-27 12:15:09 -0400

    ADD buster and stretch images

--- build.sh
+++ build.sh
@@ -2,5 +2,9 @@
 
 docker build -t stephenneal/python-flask:3.7 ./3.7/
 docker build -t stephenneal/python-flask:3.7-alpine ./3.7-alpine/
+docker build -t stephenneal/python-flask:3.7-buster ./3.7-buster/
 docker build -t stephenneal/python-flask:3.7-slim ./3.7-slim/
+docker build -t stephenneal/python-flask:3.7-slim-buster ./3.7-slim-buster/
+docker build -t stephenneal/python-flask:3.7-slim-stretch ./3.7-slim-stretch/
 docker build -t stephenneal/python-flask:3.7-slim-v2 ./3.7-slim-v2/
+docker build -t stephenneal/python-flask:3.7-stretch ./3.7-stretch/
--- push.sh
+++ push.sh
@@ -4,5 +4,9 @@ sh ./build.sh
 
 docker push stephenneal/python-flask:3.7
 docker push stephenneal/python-flask:3.7-alpine
+docker push stephenneal/python-flask:3.7-buster
 docker push stephenneal/python-flask:3.7-slim
+docker push stephenneal/python-flask:3.7-slim-buster
+docker push stephenneal/python-flask:3.7-slim-stretch
 docker push stephenneal/python-flask:3.7-slim-v2
+docker push stephenneal/python-flask:3.7-stretch
