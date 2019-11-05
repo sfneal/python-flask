bde3d00ca24f8a31e4193df6b512a47ebf5725c9
mrstephenneal <stephen.neal14@gmail.com>
2019-11-05 13:35:42 -0500

    ADD absolute directory resolving to build and push shell scripts

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-docker build -t stephenneal/python-flask:3.7 ./3.7/
-docker build -t stephenneal/python-flask:3.7-alpine ./3.7-alpine/
-docker build -t stephenneal/python-flask:3.7-buster ./3.7-buster/
-docker build -t stephenneal/python-flask:3.7-slim ./3.7-slim/
-docker build -t stephenneal/python-flask:3.7-slim-buster ./3.7-slim-buster/
-docker build -t stephenneal/python-flask:3.7-slim-stretch ./3.7-slim-stretch/
-docker build -t stephenneal/python-flask:3.7-slim-v2 ./3.7-slim-v2/
-docker build -t stephenneal/python-flask:3.7-stretch ./3.7-stretch/
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+docker build -t stephenneal/python-flask:3.7 "${DIR}"/3.7/
+docker build -t stephenneal/python-flask:3.7-alpine "${DIR}"/3.7-alpine/
+docker build -t stephenneal/python-flask:3.7-buster "${DIR}"/3.7-buster/
+docker build -t stephenneal/python-flask:3.7-slim "${DIR}"/3.7-slim/
+docker build -t stephenneal/python-flask:3.7-slim-buster "${DIR}"/3.7-slim-buster/
+docker build -t stephenneal/python-flask:3.7-slim-stretch "${DIR}"/3.7-slim-stretch/
+docker build -t stephenneal/python-flask:3.7-slim-v2 "${DIR}"/3.7-slim-v2/
+docker build -t stephenneal/python-flask:3.7-stretch "${DIR}"/3.7-stretch/
