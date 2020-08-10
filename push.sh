2722e6b7704eda1f94278b19f086793bec682f70
mrstephenneal <stephen.neal14@gmail.com>
2020-08-10 12:48:03 -0400

    CUT 3.7-slim-buster image as it was the same as 3.7-slim

--- push.sh
+++ push.sh
@@ -8,7 +8,6 @@ docker push stephenneal/python-flask:3.7
 docker push stephenneal/python-flask:3.7-alpine
 docker push stephenneal/python-flask:3.7-buster
 docker push stephenneal/python-flask:3.7-slim
-docker push stephenneal/python-flask:3.7-slim-buster
 docker push stephenneal/python-flask:3.7-slim-stretch
 docker push stephenneal/python-flask:3.7-stretch
 docker push stephenneal/python-flask:3.8-slim
