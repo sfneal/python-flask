9722201c73663fa820459993e17200c6f6e353c1
mrstephenneal <stephen.neal14@gmail.com>
2020-08-10 12:46:42 -0400

    CUT 3.7-slim-v2 image as it was the same as 3.7-slim

--- build.sh
+++ build.sh
@@ -8,7 +8,6 @@ docker build -t stephenneal/python-flask:3.7-buster "${DIR}"/3.7-buster/
 docker build -t stephenneal/python-flask:3.7-slim "${DIR}"/3.7-slim/
 docker build -t stephenneal/python-flask:3.7-slim-buster "${DIR}"/3.7-slim-buster/
 docker build -t stephenneal/python-flask:3.7-slim-stretch "${DIR}"/3.7-slim-stretch/
-docker build -t stephenneal/python-flask:3.7-slim-v2 "${DIR}"/3.7-slim-v2/
 docker build -t stephenneal/python-flask:3.7-stretch "${DIR}"/3.7-stretch/
 docker build -t stephenneal/python-flask:3.8-slim "${DIR}"/3.8-slim/
 docker build -t stephenneal/python-flask:3.9-rc-buster "${DIR}"/3.9-rc-buster/
--- push.sh
+++ push.sh
@@ -10,7 +10,6 @@ docker push stephenneal/python-flask:3.7-buster
 docker push stephenneal/python-flask:3.7-slim
 docker push stephenneal/python-flask:3.7-slim-buster
 docker push stephenneal/python-flask:3.7-slim-stretch
-docker push stephenneal/python-flask:3.7-slim-v2
 docker push stephenneal/python-flask:3.7-stretch
 docker push stephenneal/python-flask:3.8-slim
 docker push stephenneal/python-flask:3.9-rc-buster
