c0ab5538289ca18cb8e2e4fd2dae4557d7716ef2
Stephen Neal <stephen.neal14@gmail.com>
2021-03-15 12:51:42 -0400

    ADD 'deploy' section to Travis CI config for pushing Docker images to Docker Hub

--- push.sh
+++ push.sh
@@ -2,15 +2,29 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-sh "${DIR}"/build.sh
+# Optional TAG argument (if set, only the specified image will be built)
+TAG=${1:-null}
 
-docker push stephenneal/python-flask:3.7
-docker push stephenneal/python-flask:3.7-alpine
-docker push stephenneal/python-flask:3.7-buster
-docker push stephenneal/python-flask:3.7-slim
-docker push stephenneal/python-flask:3.7-slim-stretch
-docker push stephenneal/python-flask:3.7-stretch
-docker push stephenneal/python-flask:3.8-slim
-docker push stephenneal/python-flask:3.9-buster
-docker push stephenneal/python-flask:3.9-slim
-docker push stephenneal/python-flask:3.10-rc-buster
+# Check if the TAG variable is set
+if [ "$TAG" != null ]
+
+  # Only build & push one image
+  then
+    sh "${DIR}"/build.sh "${TAG}"
+    docker push stephenneal/python-flask:"${TAG}"
+
+  # Build & push all images
+  else
+    sh "${DIR}"/build.sh
+
+    docker push stephenneal/python-flask:3.7
+    docker push stephenneal/python-flask:3.7-alpine
+    docker push stephenneal/python-flask:3.7-buster
+    docker push stephenneal/python-flask:3.7-slim
+    docker push stephenneal/python-flask:3.7-slim-stretch
+    docker push stephenneal/python-flask:3.7-stretch
+    docker push stephenneal/python-flask:3.8-slim
+    docker push stephenneal/python-flask:3.9-buster
+    docker push stephenneal/python-flask:3.9-slim
+    docker push stephenneal/python-flask:3.10-rc-buster
+fi
