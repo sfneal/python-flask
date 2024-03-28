fe0a9bedb955c25db19178d4bbbf2bca0f483f57
sfneal <stephen.neal14@gmail.com>
2024-03-28 18:10:02 -0400

    CUT 3.7 slim & slim-stretch images due to base images depreciation

--- build.sh
+++ build.sh
@@ -18,8 +18,6 @@ if [ "$TAG" != null ]
     docker build -t stephenneal/python-flask:3.7 "${DIR}"/3.7/
     docker build -t stephenneal/python-flask:3.7-alpine "${DIR}"/3.7-alpine/
     docker build -t stephenneal/python-flask:3.7-buster "${DIR}"/3.7-buster/
-    docker build -t stephenneal/python-flask:3.7-slim "${DIR}"/3.7-slim/
-    docker build -t stephenneal/python-flask:3.7-slim-stretch "${DIR}"/3.7-slim-stretch/
     docker build -t stephenneal/python-flask:3.7-stretch "${DIR}"/3.7-stretch/
     docker build -t stephenneal/python-flask:3.8-slim "${DIR}"/3.8-slim/
     docker build -t stephenneal/python-flask:3.9-buster "${DIR}"/3.9-buster/
